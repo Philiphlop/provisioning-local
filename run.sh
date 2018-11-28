6f82d695d4e1a2d44cabb0d803df01537ee3ebf7
Philip Whiteside <pwsubbox@gmail.com>
2018-11-28 08:14:04 +0000

    Comment out additional actions, to allow for staged migration to Fedora base

--- run.sh
+++ run.sh
@@ -10,10 +10,10 @@ echo -e "please make sure to edit the config.json file followed by [ENTER]" && r
 
 ansible-playbook setup.yml -i HOSTS --ask-sudo-pass  --module-path ./ansible_modules --extra-vars "@config.json"
 
-dropbox start -i > /dev/null 2>&1 &
-plank > /dev/null 2>&1 &
-steam > /dev/null 2>&1 &
-emacs > /dev/null 2>&1 &
+# dropbox start -i > /dev/null 2>&1 &
+# plank > /dev/null 2>&1 &
+# steam > /dev/null 2>&1 &
+# emacs > /dev/null 2>&1 &
 
 source ~/.bashrc
 exit 0
